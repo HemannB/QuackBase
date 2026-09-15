cdfb32a9adeed4b5a8fa62e8e079b283dde4c1cc
HemannB <brunohemann@gmail.com>
2026-09-15 15:06:45 -0300

    feat: add mock user provider

---
+++ app/src/main/java/br/com/project/quackbase/mock/MockUserProvider.kt
@@ -0,0 +1,12 @@
+package br.com.project.quackbase.mock;
+import br.com.project.quackbase.model.User
+
+
+object MockUserProvider {
+    fun getUser(): User {
+        return User(
+            name = "Duck Dodgers",
+            email = "duckdoquack@duck.com"
+        )
+    }
+}
